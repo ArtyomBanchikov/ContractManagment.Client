3122f05ba8d8628986481365e21514e84f739d03
AngryKirill <mister.lolich@mail.ru>
2023-11-16 18:08:11 +0300

    Add LoginWindow and MainWindow factories

---
+++ MVVM/View/Factories/LoginWindowFactory.cs
@@ -0,0 +1,34 @@
+﻿using ContractManagment.Client.MVVM.ViewModel;
+using ContractManagment.Client.Services;
+using ContractManagment.Client.Services.XmlServices;
+using ContractManagment.Client.State.Authenticators;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContractManagment.Client.MVVM.View.Factories
+{
+    public static class LoginWindowFactory
+    {
+        public static LoginView Window { get; private set; }
+        static LoginWindowFactory()
+        {
+            IXmlService xmlService = ServiceProviderFactory.ServiceProvider.GetRequiredService<IXmlService>();
+            IAuthenticator authenticator = ServiceProviderFactory.ServiceProvider.GetRequiredService<IAuthenticator>();
+            LoginViewModel loginVM = new LoginViewModel(authenticator);
+            Window = new LoginView(loginVM, authenticator, xmlService);
+        }
+        public static LoginView NewWindow()
+        {
+            IXmlService xmlService = ServiceProviderFactory.ServiceProvider.GetRequiredService<IXmlService>();
+            IAuthenticator authenticator = ServiceProviderFactory.ServiceProvider.GetRequiredService<IAuthenticator>();
+            LoginViewModel loginVM = new LoginViewModel(authenticator);
+            Window = new LoginView(loginVM, authenticator, xmlService);
+            return Window;
+        }
+    }
+}
---
+++ MVVM/View/Factories/MainWindowFactory.cs
@@ -0,0 +1,33 @@
+﻿using ContractManagment.Client.MVVM.ViewModel;
+using ContractManagment.Client.Services;
+using ContractManagment.Client.State.Authenticators;
+using ContractManagment.Client.State.Navigators;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractManagment.Client.MVVM.View.Factories
+{
+    public static class MainWindowFactory
+    {
+        public static MainWindow Window { get; private set; }
+        static MainWindowFactory()
+        {
+            IAuthenticator authenticator = ServiceProviderFactory.ServiceProvider.GetRequiredService<IAuthenticator>();
+            INavigator navigator = ServiceProviderFactory.ServiceProvider.GetRequiredService<INavigator>();
+            MainViewModel mainVM = new MainViewModel(authenticator, navigator);
+            Window = new MainWindow(mainVM);
+        }
+        public static MainWindow NewWindow()
+        {
+            IAuthenticator authenticator = ServiceProviderFactory.ServiceProvider.GetRequiredService<IAuthenticator>();
+            INavigator navigator = ServiceProviderFactory.ServiceProvider.GetRequiredService<INavigator>();
+            MainViewModel mainVM = new MainViewModel(authenticator, navigator);
+            Window = new MainWindow(mainVM);
+            return Window;
+        }
+    }
+}
