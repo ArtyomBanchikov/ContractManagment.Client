02f91ccc9a709aec6ae4783deb4a6936777127bb
AngryKirill <mister.lolich@mail.ru>
2023-11-16 23:38:39 +0300

    Add ViewModels to new Views

---
+++ MVVM/ViewModel/ContractsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ContractManagment.Client.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractManagment.Client.MVVM.ViewModel
+{
+    public class ContractsViewModel : ViewModelBase
+    {
+    }
+}
