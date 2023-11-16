dea646b7eea122bb75348ed46d64a06826e686fd
AngryKirill <mister.lolich@mail.ru>
2023-11-16 18:16:27 +0300

    Add serviceprovider factory

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ContractManagment.Client.MVVM.Model.User;
 using ContractManagment.Client.MVVM.View;
 using ContractManagment.Client.MVVM.ViewModel;
+using ContractManagment.Client.Services;
 using ContractManagment.Client.Services.StartServices;
 using ContractManagment.Client.Services.XmlServices;
 using ContractManagment.Client.State.Authenticators;
@@ -9,6 +10,7 @@ using ContractManagment.Client.State.WebClients;
 using ContractManagment.Client.State.WebClients.ModelClients;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Windows;
@@ -22,45 +24,38 @@ namespace ContractManagment.Client
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            IServiceProvider serviceProvider = CreateServiceProvider();
-            IStartService startService = serviceProvider.GetRequiredService<IStartService>();
-            startService.Start(serviceProvider);
-            //MainViewModel mainVM = serviceProvider.GetRequiredService<MainViewModel>();
-            //mainVM.Panel = new AdminPanelViewModel();
+            IStartService startService = ServiceProviderFactory.ServiceProvider.GetRequiredService<IStartService>();
+            startService.Start();
+
+            //Window window = ServiceProviderFactory.ServiceProvider.GetRequiredService<MainWindow>();
+            //MainViewModel mainVM = (MainViewModel)window.DataContext;
             //mainVM.CurrentUser = new LoginUserModel { Name = "test", Role = "admin" };
-            //Window window = new MainWindow(mainVM);
+            //mainVM.Panel = new AdminPanelViewModel();
             //window.Show();
             base.OnStartup(e);
-        }
-        private IServiceProvider CreateServiceProvider()
-        {
-            IServiceCollection services = new ServiceCollection();
-            services.AddSingleton<IXmlService, XmlService>();
-            services.AddSingleton<IWebClient, WebClient>();
-            services.AddSingleton<IReadWriteClient<UserModel>, UserClient>();
-            services.AddSingleton<INavigator, Navigator>();
-            services.AddSingleton<IAuthenticator, Authenticator>();
-            services.AddSingleton<MainViewModel>();
-            services.AddSingleton<MainWindow>();
-            services.AddSingleton<LoginViewModel>();
-            //services.AddSingleton<LoginView>(w => new LoginView(w.GetRequiredService<LoginViewModel>(), w.GetRequiredService<IAuthenticator>(), w.GetRequiredService<MainWindow>()));
-            services.AddSingleton<LoginView>();
-            services.AddScoped<IStartService, StartService>();
-            return services.BuildServiceProvider();
         }
 
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            if(e.Exception.InnerException != null)
+            Exception exception = e.Exception;
+            while(exception.InnerException!=null && exception is AggregateException)
             {
-                if(e.Exception.InnerException is SocketException || e.Exception.InnerException is HttpRequestException)
+                exception = exception.InnerException;
+            }
+            if(exception is HttpRequestException)
+            {
+                if(((HttpRequestException)exception).StatusCode == System.Net.HttpStatusCode.Unauthorized)
                 {
-                    MessageBox.Show("Не удалось подключиться к серверу");
+                    MessageBox.Show("Ошибка авторизации");
                 }
+            }
+            else if (exception is SocketException || exception is HttpRequestException)
+            {
+                MessageBox.Show("Не удалось подключиться к серверу");
             }
             else
             {
-                MessageBox.Show(e.Exception.Message);
+                MessageBox.Show(exception.Message);
             }
             e.Handled = true;
         }
