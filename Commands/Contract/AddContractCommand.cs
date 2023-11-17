8a15f6e0a27bef4ebe31e0ad27b15172a508c745
AngryKirill <mister.lolich@mail.ru>
2023-11-17 21:37:40 +0300

    Add contract command placeholders

---
+++ Commands/Contract/AddContractCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractManagment.Client.Commands.Contract
+{
+    public class AddContractCommand : AsyncBaseCommand
+    {
+        protected async override Task ExecuteAsync(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
