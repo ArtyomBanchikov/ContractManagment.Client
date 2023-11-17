659bb602b03cf13077606fecba37d5838bd47e9d
AngryKirill <mister.lolich@mail.ru>
2023-11-17 16:02:25 +0300

    Add client for contract model

--- State/WebClients/ModelClients/ContractClient.cs
+++ State/WebClients/ModelClients/ContractClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContractManagment.Client.MVVM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace ContractManagment.Client.State.WebClients.ModelClients
 {
-    internal class ContractClient
+    public class ContractClient : IReadWriteClient<ContractModel>
     {
+        public Task Create(ContractModel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ContractModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContractModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(ContractModel obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
