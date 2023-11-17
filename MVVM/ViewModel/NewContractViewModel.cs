2c88ac4cab58cc5fe99fd2f01f6c3f0280c01d6a
AngryKirill <mister.lolich@mail.ru>
2023-11-17 22:29:24 +0300

    Add NewContract view and viewmodel

---
+++ MVVM/ViewModel/NewContractViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ContractManagment.Client.Commands.Contract;
+using ContractManagment.Client.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ContractManagment.Client.MVVM.ViewModel
+{
+    public class NewContractViewModel : ViewModelBase
+    {
+        public ICommand AddNewContractCommand { get; set; }
+        public NewContractViewModel()
+        {
+            AddNewContractCommand = new AddContractCommandAsync();
+        }
+    }
+}
