c3ec4ea78a49881ed2b22973b605b86ac225e41e
AngryKirill <mister.lolich@mail.ru>
2023-11-17 17:18:55 +0300

    Update keys view

--- Commands/UpdateCurrentViewModelCommandAsync.cs
+++ Commands/UpdateCurrentViewModelCommandAsync.cs
@@ -1,8 +1,14 @@
-﻿using ContractManagment.Client.MVVM.ViewModel;
+﻿using ContractManagment.Client.MVVM.Model;
+using ContractManagment.Client.MVVM.Model.Client;
+using ContractManagment.Client.MVVM.ViewModel;
 using ContractManagment.Client.Services;
 using ContractManagment.Client.State.Navigators;
+using ContractManagment.Client.State.WebClients;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace ContractManagment.Client.Commands
 {
@@ -29,6 +35,13 @@ namespace ContractManagment.Client.Commands
                         break;
                     case ViewType.Keys:
                         _navigator.CurrentViewModel = ServiceProviderFactory.ServiceProvider.GetRequiredService<KeysViewModel>();
+                        if(((KeysViewModel)_navigator.CurrentViewModel).Keys == null)
+                        {
+                            IReadWriteClient<KeyModel> keyClient = ServiceProviderFactory.ServiceProvider.GetRequiredService<IReadWriteClient<KeyModel>>();
+                            ((KeysViewModel)_navigator.CurrentViewModel).Keys = new ObservableCollection<KeyModel>();
+                            List<KeyModel> keys = await keyClient.GetAll();
+                            keys.ForEach(key => ((KeysViewModel)_navigator.CurrentViewModel).Keys.Add(key));
+                        }
                         break;
                     case ViewType.Clients:
                         _navigator.CurrentViewModel = ServiceProviderFactory.ServiceProvider.GetRequiredService<ClientsViewModel>();
--- MVVM/ViewModel/KeysViewModel.cs
+++ MVVM/ViewModel/KeysViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using ContractManagment.Client.Core;
+using ContractManagment.Client.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContractManagment.Client.MVVM.ViewModel
 {
-    public class KeysViewModel : ViewModelBase
+    public class KeysViewModel : ViewModelBase  
     {
+        public ObservableCollection<KeyModel> Keys { get; set; }
     }
 }
