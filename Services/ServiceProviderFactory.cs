f2d83e577d0b0f0c2156f9dbe6b4adbe3509d4b7
AngryKirill <mister.lolich@mail.ru>
2023-11-17 16:07:18 +0300

    Register model clients services

--- Services/ServiceProviderFactory.cs
+++ Services/ServiceProviderFactory.cs
@@ -1,5 +1,4 @@
 ﻿using ContractManagment.Client.MVVM.Model.User;
-using ContractManagment.Client.MVVM.View;
 using ContractManagment.Client.MVVM.ViewModel;
 using ContractManagment.Client.Services.StartServices;
 using ContractManagment.Client.Services.XmlServices;
@@ -9,10 +8,11 @@ using ContractManagment.Client.State.WebClients.ModelClients;
 using ContractManagment.Client.State.WebClients;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ContractManagment.Client.MVVM.Model.Client;
+using ContractManagment.Client.State.WebClients.ModelClients.Client;
+using ContractManagment.Client.MVVM.Model.Post;
+using ContractManagment.Client.State.WebClients.ModelClients.Post;
+using ContractManagment.Client.MVVM.Model;
 
 namespace ContractManagment.Client.Services
 {
@@ -25,6 +25,14 @@ namespace ContractManagment.Client.Services
             IServiceCollection services = new ServiceCollection();
             services.AddSingleton<IXmlService, XmlService>();
             services.AddSingleton<IWebClient, WebClient>();
+            services.AddSingleton<IReadClient<AdditionalParameterModel>, AdditionalParameterClient>();
+            services.AddSingleton<IReadClient<ClientAddParamModel>, ClientAddParamClient>();
+            services.AddSingleton<IReadClient<ClientModel>, ClientClient>();
+            services.AddSingleton<IReadClient<PostModel>, PostClient>();
+            services.AddSingleton<IReadClient<PostMetaModel>, PostMetaClient>();
+            services.AddSingleton<IReadWriteClient<ContractModel>, ContractClient>();
+            services.AddSingleton<IReadWriteClient<ContractKeyModel>, ContractKeyClient>();
+            services.AddSingleton<IReadWriteClient<KeyModel>, KeyClient>();
             services.AddSingleton<IReadWriteClient<UserModel>, UserClient>();
             services.AddSingleton<INavigator, Navigator>();
             services.AddSingleton<IAuthenticator, Authenticator>();
