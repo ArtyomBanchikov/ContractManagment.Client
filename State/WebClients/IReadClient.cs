509233589fa880abcf23f9cbd3fdb99e29691e04
AngryKirill <mister.lolich@mail.ru>
2023-11-17 15:59:39 +0300

    delete client property from read and readwrite client interfaces

--- State/WebClients/IReadClient.cs
+++ State/WebClients/IReadClient.cs
@@ -5,7 +5,6 @@ namespace ContractManagment.Client.State.WebClients
 {
     public interface IReadClient<T> where T : class
     {
-        public IWebClient Client { get; }
         public Task<T> GetById(int id);
         public Task<List<T>> GetAll();
     }
--- State/WebClients/IReadWriteClient.cs
+++ State/WebClients/IReadWriteClient.cs
@@ -5,7 +5,6 @@ namespace ContractManagment.Client.State.WebClients
 {
     public interface IReadWriteClient<T> where T : class
     {
-        public IWebClient Client { get; }
         public Task<T> GetById(int id);
         public Task<List<T>> GetAll();
         public Task DeleteById(int id);
