d2bc0c6681bf7c6b83ba927b4c22239bf7bb072a
AngryKirill <mister.lolich@mail.ru>
2023-11-17 13:55:46 +0300

    Update iread and ireadwrite clients for async

--- State/WebClients/IReadClient.cs
+++ State/WebClients/IReadClient.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ContractManagment.Client.State.WebClients
@@ -9,7 +6,7 @@ namespace ContractManagment.Client.State.WebClients
     public interface IReadClient<T> where T : class
     {
         public IWebClient Client { get; }
-        public T GetById(int id);
-        public List<T> GetAll();
+        public Task<T> GetById(int id);
+        public Task<List<T>> GetAll();
     }
 }
--- State/WebClients/IReadWriteClient.cs
+++ State/WebClients/IReadWriteClient.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ContractManagment.Client.State.WebClients
@@ -9,10 +6,10 @@ namespace ContractManagment.Client.State.WebClients
     public interface IReadWriteClient<T> where T : class
     {
         public IWebClient Client { get; }
-        public T GetById(int id);
-        public List<T> GetAll();
-        public void DeleteById(int id);
-        public void Create(T obj);
-        public void Update(T obj);
+        public Task<T> GetById(int id);
+        public Task<List<T>> GetAll();
+        public Task DeleteById(int id);
+        public Task Create(T obj);
+        public Task Update(T obj);
     }
 }
