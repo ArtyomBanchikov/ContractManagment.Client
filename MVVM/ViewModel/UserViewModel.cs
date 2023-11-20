1670b670ed19f0827e844b91f111a615b3020c14
AngryKirill <mister.lolich@mail.ru>
2023-11-20 17:47:08 +0300

    Add User View and ViewModel with basic elements

--- MVVM/ViewModel/UserViewModel.cs
+++ MVVM/ViewModel/UserViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using ContractManagment.Client.Commands;
 using ContractManagment.Client.Core;
+using ContractManagment.Client.MVVM.Model.User;
 using ContractManagment.Client.Services;
 using ContractManagment.Client.State.Navigators;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace ContractManagment.Client.MVVM.ViewModel
@@ -10,9 +12,11 @@ namespace ContractManagment.Client.MVVM.ViewModel
     public class UserViewModel : ViewModelBase
     {
         public ICommand UpdateCurrentViewModel {  get; set; }
+        public ObservableCollection<UserModel> Users { get; set; }
         public UserViewModel()
         {
             UpdateCurrentViewModel = new UpdateCurrentViewModelCommandAsync(ServiceProviderFactory.ServiceProvider.GetRequiredService<INavigator>());
+            Users = new ObservableCollection<UserModel>();
         }
     }
 }
