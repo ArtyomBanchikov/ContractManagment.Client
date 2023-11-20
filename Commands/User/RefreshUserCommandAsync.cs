06af136d9ccbd545e20737cb9a15a8329c008854
AngryKirill <mister.lolich@mail.ru>
2023-11-20 18:17:20 +0300

    Add refreshuser command

---
+++ Commands/User/RefreshUserCommandAsync.cs
@@ -0,0 +1,32 @@
+﻿using ContractManagment.Client.MVVM.Model;
+using ContractManagment.Client.MVVM.Model.User;
+using ContractManagment.Client.MVVM.ViewModel;
+using ContractManagment.Client.Services;
+using ContractManagment.Client.State.WebClients;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractManagment.Client.Commands.User
+{
+    class RefreshUserCommandAsync : AsyncBaseCommand
+    {
+        private UserViewModel _userVM;
+
+        public RefreshUserCommandAsync(UserViewModel userVM)
+        {
+            _userVM = userVM;
+        }
+
+        protected override async Task ExecuteAsync(object parameter)
+        {
+            IReadWriteClient<UserModel> userClient = ServiceProviderFactory.ServiceProvider.GetRequiredService<IReadWriteClient<UserModel>>();
+            _userVM.Users.Clear();
+            List<UserModel> users = await userClient.GetAll();
+            users.ForEach(user => _userVM.Users.Add(user));
+        }
+    }
+}
--- MVVM/ViewModel/UserViewModel.cs
+++ MVVM/ViewModel/UserViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ContractManagment.Client.Commands;
+using ContractManagment.Client.Commands.User;
 using ContractManagment.Client.Core;
 using ContractManagment.Client.MVVM.Model.User;
 using ContractManagment.Client.Services;
@@ -11,11 +12,13 @@ namespace ContractManagment.Client.MVVM.ViewModel
 {
     public class UserViewModel : ViewModelBase
     {
+        public ICommand RefreshUserCommand { get; set; }
         public ICommand UpdateCurrentViewModel {  get; set; }
         public ObservableCollection<UserModel> Users { get; set; }
         public UserViewModel()
         {
             UpdateCurrentViewModel = new UpdateCurrentViewModelCommandAsync(ServiceProviderFactory.ServiceProvider.GetRequiredService<INavigator>());
+            RefreshUserCommand = new RefreshUserCommandAsync(this);
         }
     }
 }
