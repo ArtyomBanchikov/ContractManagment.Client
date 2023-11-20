faa734fa80241b8590be89e4e93ec17bc68527e4
AngryKirill <mister.lolich@mail.ru>
2023-11-20 20:37:03 +0300

    Add removing user

--- Commands/User/DeleteUserCommandAsync.cs
+++ Commands/User/DeleteUserCommandAsync.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using ContractManagment.Client.MVVM.Model;
+using ContractManagment.Client.MVVM.Model.User;
+using ContractManagment.Client.MVVM.ViewModel;
+using ContractManagment.Client.Services;
+using ContractManagment.Client.State.WebClients;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ContractManagment.Client.Commands.User
 {
-    class DeleteUserCommandAsync
+    class DeleteUserCommandAsync : AsyncBaseCommand
     {
+        private UserViewModel _userVM;
+
+        public DeleteUserCommandAsync(UserViewModel userVM)
+        {
+            _userVM = userVM;
+        }
+
+        protected override async Task ExecuteAsync(object parameter)
+        {
+            if (_userVM.SelectedUser != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите удалить ключ?", "", MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        UserModel deletedUser = _userVM.SelectedUser;
+                        IReadWriteClient<UserModel> userClient = ServiceProviderFactory.ServiceProvider.GetRequiredService<IReadWriteClient<UserModel>>();
+                        await userClient.DeleteById(deletedUser.Id);
+                        _userVM.SelectedUser = null;
+                        _userVM.Users.Remove(deletedUser);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+                MessageBox.Show("Выберите пользователя для удаления");
+        }
     }
 }
--- MVVM/ViewModel/UserViewModel.cs
+++ MVVM/ViewModel/UserViewModel.cs
@@ -12,13 +12,25 @@ namespace ContractManagment.Client.MVVM.ViewModel
 {
     public class UserViewModel : ViewModelBase
     {
+        private UserModel _selectedUser;
+        public UserModel SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                OnPropertyChanged(nameof(SelectedUser));
+            }
+        }
         public ICommand RefreshUserCommand { get; set; }
         public ICommand UpdateCurrentViewModel {  get; set; }
+        public ICommand DeleteUserCommand { get; set; }
         public ObservableCollection<UserModel> Users { get; set; }
         public UserViewModel()
         {
             UpdateCurrentViewModel = new UpdateCurrentViewModelCommandAsync(ServiceProviderFactory.ServiceProvider.GetRequiredService<INavigator>());
             RefreshUserCommand = new RefreshUserCommandAsync(this);
+            DeleteUserCommand = new DeleteUserCommandAsync(this);
         }
     }
 }
