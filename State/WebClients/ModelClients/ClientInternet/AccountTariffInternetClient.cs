0cc39d28da653d990fc59447387939933f930aaa
AngryKirill <mister.lolich@mail.ru>
2023-11-29 17:37:02 +0300

    add client tariffs and add NCamModul and connect date to clients

---
+++ State/WebClients/ModelClients/ClientInternet/AccountTariffInternetClient.cs
@@ -0,0 +1,28 @@
+﻿using ContractManagment.Client.MVVM.Model.ClientInternet;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ContractManagment.Client.State.WebClients.ModelClients.ClientInternet
+{
+    public class AccountTariffInternetClient : IReadClient<AccountTariffInternetModel>
+    {
+        private IWebClient _webClient;
+
+        public AccountTariffInternetClient(IWebClient webClient)
+        {
+            _webClient = webClient;
+        }
+
+        public async Task<List<AccountTariffInternetModel>> GetAll()
+        {
+            List<AccountTariffInternetModel> list = await _webClient.Client.GetFromJsonAsync<List<AccountTariffInternetModel>>("/AccountTariffInternet");
+            return list;
+        }
+
+        public async Task<AccountTariffInternetModel> GetById(int id)
+        {
+            return await _webClient.Client.GetFromJsonAsync<AccountTariffInternetModel>($"/AccountTariffInternet/{id}");
+        }
+    }
+}
