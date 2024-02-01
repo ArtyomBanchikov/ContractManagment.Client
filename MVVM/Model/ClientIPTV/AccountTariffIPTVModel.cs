20726e1a1ba181fab7557450bfae95a5f80b903a
AngryKirill <mister.lolich@mail.ru>
2024-02-01 16:07:01 +0300

    Add IPTV clients models

---
+++ MVVM/Model/ClientIPTV/AccountTariffIPTVModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContractManagment.Client.MVVM.Model.ClientIPTV
+{
+    public class AccountTariffIPTVModel
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int TariffId { get; set; }
+        public TariffIPTVModel? Tariff { get; set; }
+        public int IsDeleted { get; set; }
+    }
+}
