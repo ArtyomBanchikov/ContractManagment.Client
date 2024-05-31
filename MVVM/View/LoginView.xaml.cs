9521f8775ee33c7f41d6fa7a354fe2bbfc11835f
AngryKirill <mister.lolich@mail.ru>
2024-05-31 23:30:02 +0300

    Delete Login_click and move its funcionality to LoginCommand

--- MVVM/View/LoginView.xaml.cs
+++ MVVM/View/LoginView.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using ContractManagment.Client.MVVM.View.Factories;
 using ContractManagment.Client.MVVM.ViewModel;
-using ContractManagment.Client.Services;
 using ContractManagment.Client.Services.XmlServices;
 using ContractManagment.Client.State.Authenticators;
-using Microsoft.AspNet.Identity;
-using System;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -26,48 +22,16 @@ namespace ContractManagment.Client.MVVM.View
             _authenticator = authenticator;
             _xmlProvider = xmlProvider;
             InitializeComponent();
+
+            viewModel.LoginSuccessful += (sender, e) =>
+            {
+                MainWindow window = MainWindowFactory.NewWindow();
+
+                window.Show();
+                Close();
+                Application.Current.MainWindow = window;
+            };
         }
-
-        private async void Login_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(LoginBox.Text))
-            {
-                MessageBox.Show("Введите имя пользователя");
-                LoginBox.Focus();
-            }
-            else if(string.IsNullOrEmpty(password_box.Password))
-            {
-                MessageBox.Show("Введите пароль");
-                password_box.Focus();
-            }
-            else 
-            {
-                if (LoginCommand != null)
-                {
-                    LoginCommand.Execute(password_box.Password);
-                    if (_authenticator != null && _authenticator.IsLoggedIn)
-                    {
-                        if ((bool)RememberCheck.IsChecked)
-                        {
-                            _xmlProvider.IsRemember = true;
-                            _xmlProvider.Token = _authenticator.CurrentUser.Token;
-                        }
-                        else if(_xmlProvider.IsRemember)
-                        {
-                            _xmlProvider.IsRemember=false;
-                            _xmlProvider.Token = "";
-                        }
-                        _xmlProvider.LastLogin = LoginBox.Text;
-                        MainWindow window = MainWindowFactory.NewWindow();
-
-                        window.Show();
-                        this.Close();
-                        Application.Current.MainWindow = window;
-                    }
-                }
-            }
-        }
-
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if(e.LeftButton == MouseButtonState.Pressed)
