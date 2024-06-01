3aa78b2603586b6bd303d6c3f8721a1a6671fc7e
ArtyomBanchikov <mister.lolich@mail.ru>
2024-06-01 20:54:10 +0300

    Make WindowOpen async

--- Services/StartServices/IStartService.cs
+++ Services/StartServices/IStartService.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 
 namespace ContractManagment.Client.Services.StartServices
 {
     public interface IStartService
     {
         void Start();
-        void WindowOpen();
+        Task WindowOpen();
     }
 }
